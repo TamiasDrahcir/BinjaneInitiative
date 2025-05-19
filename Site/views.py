1903f35e8630e2b3107a3447beeb65e704482940
TamiasDrahcir <richardwyiu@outlook.com>
2025-05-20 00:08:01 +0800

    Commit for Third Time

---
+++ Site/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request,'Site/home.html')
+
+def homeeng(request):
+    return render(request,'Site/homeeng.html')
+
+def about(request):
+    return render(request,'Site/about.html')
+
+def service(request):
+    return render(request,'Site/service.html')
+
+def contact(request):
+    return render(request,'Site/contact.html')
