827c6b577772f45dc6cc1726d84ba9c58cdcac1f
TamiasDrahcir <richardwyiu@outlook.com>
2025-05-20 16:53:41 +0800

    Update May 20

--- Site/urls.py
+++ Site/urls.py
@@ -5,6 +5,11 @@ urlpatterns = [
     path('home/eng=off',views.home,name="home"),
     path('home/eng=on', views.homeeng,name="homeeng"),
     path('about/eng=off',views.about,name="about"),
+    path('about/eng=on', views.abouteng,name="abouteng"),
     path('service/eng=off',views.service,name="service"),
-    path('contact/eng=off',views.contact,name="contact")
+    path('service/eng=on', views.serviceeng,name="serviceeng"),
+    path('contact/eng=off',views.contact,name="contact"),
+    path('contact/eng=on',views.contacteng,name="contacteng"),
+    path('team/eng=off',views.team,name="team"),
+    path('team/eng=on',views.teameng,name="teameng"),
 ]
--- Site/views.py
+++ Site/views.py
@@ -10,8 +10,23 @@ def homeeng(request):
 def about(request):
     return render(request,'Site/about.html')
 
+def abouteng(request):
+    return render(request,'Site/abouteng.html')
+
 def service(request):
     return render(request,'Site/service.html')
 
+def serviceeng(request):
+    return render(request,'Site/serviceeng.html')
+
 def contact(request):
     return render(request,'Site/contact.html')
+
+def contacteng(request):
+    return render(request,'Site/contacteng.html')
+
+def team(request):
+    return render(request,'Site/team.html')
+
+def teameng(request):
+    return render(request,'Site/teameng.html')
